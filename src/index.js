a261f2453330156baf41ee923a55e916533e3dda
Rupesh Jaiswal <RupeshJ@citiustech.com>
2018-10-05 19:31:16 +0530

    section 2 udemy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 // Create a new component. this component should produce some HTML
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1n027DP0_5Ss';
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        };
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 //Take this component's generated HTML and put it on the page(in the DOM)
 
